72effde53facaa3f8ac157a275929df9a72c10a3
Nipun Lokuwithanage <nipun.lok@gmail.com>
2026-03-07 23:58:09 +1100

    q3 summary of current research

--- Q3/Question-3.cc
+++ Q3/Question-3.cc
@@ -15,10 +15,26 @@ double dist(const Point& a, const Point& b) {
 
 double len(const Point& p) {return std::hypot(p.x, p.y);}
 
-
 // Please also try document your thought process and your code as you go!
 int main(void) {
     std::ifstream cones{"track.csv"};
-    Point point{1,1};
+
+    cones.close();
     return 0;
 }
+
+/*
+Unable to finish on time. Here are my current considered approaches
+
+-   use potential field with track points as repulsors
+    mark near-stationary trajectories as centreline
+    https://upload.wikimedia.org/wikipedia/commons/3/3d/Cyberbotics%27_Robot_Curriculum.pdf
+
+-   as per hints.txt: use delauney triangulation to generate voronoi diagram
+    use midpoint of edges as markers for the centreline
+    use cubic spline interpolation between marked points to generate smooth
+    centreline
+    https://www.cis.upenn.edu/~cis6100/convex8.pdf
+    https://cgi.cse.unsw.edu.au/~blair/pub/iros03.pdf
+
+*/
